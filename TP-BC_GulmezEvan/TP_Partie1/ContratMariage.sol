62b12ff59732ffe6d460590531556f636235b58e
Luka Gulmez <you@example.com>
2022-06-23 11:25:55 +0200

    TotallySpiesToken Comments

--- TP-BC_GulmezEvan/TP_Partie1/ContratMariage.sol
+++ TP-BC_GulmezEvan/TP_Partie1/ContratMariage.sol
@@ -11,7 +11,7 @@ function Married(string memory NameFirst,string memory NameSecond) public {
     FullNameFirst = NameFirst; // Name of the First person
     FullNameSecond = NameSecond; // Name of the Second person
 }
-
+// Function that returns names and status of married
 function Status() public view returns (string memory) {
     // Concat the names of the married
     string memory Marrystatus = string.concat(FullNameFirst, "married to",FullNameSecond);
--- TP-BC_GulmezEvan/TP_Partie1/TotallySpiesToken.sol
+++ TP-BC_GulmezEvan/TP_Partie1/TotallySpiesToken.sol
@@ -15,11 +15,11 @@ contract ERC20Interface {
     // Transfer approvement
     function approve(address spender, uint tokens) public returns (bool success);
     //Transfer tokens between two wallet address
-    
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
-    // 
+    
+    // Event Transfer 
     event Transfer(address indexed from, address indexed to, uint tokens);
-    //  
+    // Event Approval
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
@@ -32,21 +32,24 @@ contract SafeMath {
         c = a + b;
         require(c >= a);
     }
+    //
     function safeSub(uint a, uint b) public pure returns (uint c) {
         require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0);
         c = a / b;
     }
 }
 
-
+// Token inherits of ERC20Interface and SafeMath
 contract TotallySpiesToken is ERC20Interface, SafeMath {
-    string public name;
-    string public symbol;
+    string public name; // Token Name
+    string public symbol; // Token Symbol
     uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
 
-    uint256 public _totalSupply;
+    uint256 public _totalSupply; // maximum numbers of tokens
 
+    //mapping stored the balance of the address
     mapping(address => uint) balances;
+    //stored per address the balance allowed 
     mapping(address => mapping(address => uint)) allowed;
 
 // ----------------------------------------------------------------------------
@@ -55,40 +58,40 @@ contract TotallySpiesToken is ERC20Interface, SafeMath {
 //  * Initializes contract with initial supply tokens to the creator of the contract
 // ----------------------------------------------------------------------------
     constructor() public {
-        name = "TotallySpies";
-        symbol = "SPY";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
+        name = "TotallySpies"; // Token Name
+        symbol = "SPY"; // Token Symbol
+        decimals = 18; 
+        _totalSupply = 100000000000000000000000000; // Max Suplly
 
-        balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        balances[msg.sender] = _totalSupply; // The balance of the owner take all the tokens
+        emit Transfer(address(0), msg.sender, _totalSupply);// the first address is the owner 
     }
-
+    //
     function totalSupply() public view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
-
+    //Balance of the owner
     function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
-
+    //
     function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
-
+    // Transaction Approvement
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
-
+    //Transfer token to an other account
     function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
-
+    //Transfer token to an different account to an other account
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
