3df2a48246de2c8c84c6e265cee86415b3fd3ec5
Luka Gulmez <you@example.com>
2022-06-23 11:02:40 +0200

    Marry Simply

--- TP-BC_GulmezEvan/TP_Partie1/ContratMariage.sol
+++ TP-BC_GulmezEvan/TP_Partie1/ContratMariage.sol
@@ -1,21 +1,21 @@
-pragma solidity 0.5.17;
+pragma solidity 0.8.15;
 
 contract Marry{
 
     string public FullNameFirst; // Name of the First person
     string public FullNameSecond; // Name of the Second person
 
+
 // Function which marry two people  
 function Married(string memory NameFirst,string memory NameSecond) public {
-    FullNameFirst = NameFirst;
-    FullNameSecond = NameSecond;
+    FullNameFirst = NameFirst; // Name of the First person
+    FullNameSecond = NameSecond; // Name of the Second person
 }
 
 function Status() public view returns (string memory) {
-    string memory Marrystatus;
     // Concat the names of the married
-    Marrystatus = string.concat(FullNameFirst, "married to",FullNameSecond);
-}
+    string memory Marrystatus = string.concat(FullNameFirst, "married to",FullNameSecond);
 // Return the Marry Status
 return Marrystatus;
+}
 }
